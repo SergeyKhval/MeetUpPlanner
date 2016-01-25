4d4555ef2e79f4945b186f78860d789d5a54c8a4
Sergey Khval <sergeykhval@gmail.com>
2016-01-25 11:21:54 +0300

    Scaffolded Meetup Planner app

---
+++ app/scripts/meetUpPlanner.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('meetUpPlanner', ['ngSanitize', 'firebase', 'ngResource', 'ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+;
