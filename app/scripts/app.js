0871e8952a34ce01d6980b7a09151a36731686d0
Sergey Khval <sergeykhval@gmail.com>
2016-01-27 23:12:22 +0300

    refactored project to use grunt-angularfire seed

---
+++ app/scripts/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name meetUpPlannerApp
+ * @description
+ * # meetUpPlannerApp
+ *
+ * Main module of the application.
+ */
+angular.module('meetUpPlannerApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.ref',
+    'firebase.auth'
+  ]);
