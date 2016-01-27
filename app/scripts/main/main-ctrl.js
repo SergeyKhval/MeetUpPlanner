34b5fedaeb0e83d2847373a747fd5be8f7d07206
SergeyKhval <sergeykhval@gmail.com>
2016-01-27 17:07:27 +0300

    Added firebase link and purged main view

--- app/scripts/main/main-ctrl.js
+++ app/scripts/main/main-ctrl.js
@@ -5,9 +5,9 @@
 angular.module('meetUpPlanner')
   .controller('MainCtrl', function ($scope, $firebase) {
   	// now we can use $firebase to synchronize data between clients and the server!
-    var ref = new Firebase('https://<your-firebase>.firebaseio.com/');
+    var ref = new Firebase('https://udacity-web-form.firebaseio.com/');
     var sync = $firebase(ref);
-    
+
     $scope.awesomeThings = [
       {
         'key': 'angular',
