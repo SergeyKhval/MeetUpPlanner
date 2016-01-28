fca35fcc1c9990086a9fcd8c9f97d10c36a88841
Sergey Khval <sergeykhval@gmail.com>
2016-01-28 23:05:17 +0300

    Included bootstrap material design and marked up basic event creation form

---
+++ app/scripts/controllers/addevent.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name meetUpPlannerApp.controller:AddeventCtrl
+ * @description
+ * # AddeventCtrl
+ * Controller of the meetUpPlannerApp
+ */
+angular.module('meetUpPlannerApp')
+  .controller('AddeventCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
