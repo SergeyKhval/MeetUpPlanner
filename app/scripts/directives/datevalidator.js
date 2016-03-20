b9aeea60cf7061ef7b1aa75f7207150bb33bafb3
Sergey Khval <sergeykhval@gmail.com>
2016-03-20 12:00:16 +0300

    #4 added validation messages for date and time

---
+++ app/scripts/directives/datevalidator.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name meetUpPlannerApp.directive:datevalidator
+ * @description
+ * # datevalidator
+ */
+angular.module('meetUpPlannerApp')
+  .directive('datevalidator', function () {
+    return {
+      require: 'ngModel',
+      link: function (scope, elem, attr, ngModel) {
+        function validate(value) {
+          if (value !== undefined && value != null) {
+            ngModel.$setValidity('badDate', true);
+            if (value instanceof Date) {
+              var d = Date.parse(value);
+              // it is a date
+              if (isNaN(d)) {
+                ngModel.$setValidity('badDate', false);
+              }
+            } else {
+              var myPattern = new RegExp(/^([0-9]{2})\.([0-9]{2})\.([0-9]{4})$/);
+              if (value != '' && !myPattern.test(value)) {
+                ngModel.$setValidity('badDate', false);
+              }
+            }
+          }
+        }
+
+        scope.$watch(function () {
+          return ngModel.$viewValue;
+        }, validate);
+      }
+    };
+  });
